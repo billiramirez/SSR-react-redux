c14586ca0c79f1124a8378631d09b812022f50ea
Billi Ramirez <ramirezbilly42@outlook.com>
2019-08-19 13:51:10 -0600

    Refactor with node-external

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
+
 
 const config = {
   // Inform Webpack that we're building a bundle for nodejs, rather thatn for the browser
@@ -10,11 +12,12 @@ const config = {
   entry: './src/index.js',
 
   // tell webpack where to put the output file that is generated
-
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  externals: [webpackNodeExternals()] //tell webpack not bundle any library that is in node_modules folder, this is just for SERVER bundle
 }
 
 module.exports = merge(baseConfig, config);
