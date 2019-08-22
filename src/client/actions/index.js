e4d351d5fc8605a3791d30b5bad8f5e779199600
Billi Ramirez <ramirezbilly42@outlook.com>
2019-08-22 16:11:28 -0600

    styling and auth flow

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,4 +1,5 @@
 export const FETCH_USERS =  'fetch_users';
+export const FETCH_CURRENT_USER = 'fetch_current_user';
 
 export const fetchUsers = () => async (dispatch, getState, api) => {
   const res = await api.get('/users');
@@ -7,3 +8,12 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
     payload: res
   });
 };
+
+export const fetchCurrentUser = () => async (dispatch, getState, api) =>{
+  const res = await api.get('/current_user');
+  dispatch({
+    type: FETCH_CURRENT_USER,
+    payload: res
+  });
+}
+
